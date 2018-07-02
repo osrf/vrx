df1a9d9d396b02af1128ebbd2362394671488453
Kevin Allen <kallen@osrfoundation.org>
2018-07-02 11:52:58 -0700

    Style fixes for light_buoy_controller

--- robotx_gazebo/src/light_buoy_controller.cc
+++ robotx_gazebo/src/light_buoy_controller.cc
@@ -3,7 +3,7 @@
 // Register plugin with gazebo
 GZ_REGISTER_MODEL_PLUGIN(LightBuoyController)
 
-std_msgs::ColorRGBA LightBuoyController::color(double r, double g, double b, double a)
+std_msgs::ColorRGBA LightBuoyController::CreateColor(double r, double g, double b, double a)
 {
   static std_msgs::ColorRGBA color;
   color.r = r;
@@ -13,11 +13,11 @@ std_msgs::ColorRGBA LightBuoyController::color(double r, double g, double b, dou
   return color;
 }
 
-const std_msgs::ColorRGBA LightBuoyController::RED    = color(1.0, 0.0, 0.0, 1.0);
-const std_msgs::ColorRGBA LightBuoyController::GREEN  = color(0.0, 1.0, 0.0, 1.0);
-const std_msgs::ColorRGBA LightBuoyController::BLUE   = color(0.0, 0.0, 1.0, 1.0);
-const std_msgs::ColorRGBA LightBuoyController::YELLOW = color(1.0, 1.0, 0.0, 1.0);
-const std_msgs::ColorRGBA LightBuoyController::OFF    = color(0.0, 0.0, 0.0, 1.0);
+const std_msgs::ColorRGBA LightBuoyController::RED    = CreateColor(1.0, 0.0, 0.0, 1.0);
+const std_msgs::ColorRGBA LightBuoyController::GREEN  = CreateColor(0.0, 1.0, 0.0, 1.0);
+const std_msgs::ColorRGBA LightBuoyController::BLUE   = CreateColor(0.0, 0.0, 1.0, 1.0);
+const std_msgs::ColorRGBA LightBuoyController::YELLOW = CreateColor(1.0, 1.0, 0.0, 1.0);
+const std_msgs::ColorRGBA LightBuoyController::OFF    = CreateColor(0.0, 0.0, 0.0, 1.0);
 const LightBuoyController::colors_t LightBuoyController::COLORS[5] = {LightBuoyController::colors_t(RED,    "RED"),
                                                                       LightBuoyController::colors_t(GREEN,  "GREEN"),
                                                                       LightBuoyController::colors_t(BLUE,   "BLUE"),
@@ -41,49 +41,51 @@ void LightBuoyController::Load(gazebo::physics::ModelPtr _parent, sdf::ElementPt
   else 
     ROS_INFO_NAMED("light_bouy_controller", "missing <robotNamespace>, defaulting to %s", ns.c_str());
 
-  nh_ = ros::NodeHandle(ns);
+  this->nh = ros::NodeHandle(ns);
 
   // Create publisher to set color on each panel 
-  panel_pubs_[0] = nh_.advertise<std_msgs::ColorRGBA>("panel1", 1);
-  panel_pubs_[1] = nh_.advertise<std_msgs::ColorRGBA>("panel2", 1);
-  panel_pubs_[2] = nh_.advertise<std_msgs::ColorRGBA>("panel3", 1);
+  this->panelPubs[0] = this->nh.advertise<std_msgs::ColorRGBA>("panel1", 1);
+  this->panelPubs[1] = this->nh.advertise<std_msgs::ColorRGBA>("panel2", 1);
+  this->panelPubs[2] = this->nh.advertise<std_msgs::ColorRGBA>("panel3", 1);
 
   // Generate random initial pattern
   std::string initial;
-  changePattern(initial);
+  ChangePattern(initial);
 
-  change_pattern_server_ = nh_.advertiseService("new_pattern", &LightBuoyController::changePattern, this);
+  this->changePatternServer = this->nh.advertiseService("new_pattern", &LightBuoyController::ChangePattern, this);
 
-  timer_ = nh_.createTimer(ros::Duration(1.0), &LightBuoyController::incrementState, this);
+  this->timer = this->nh.createTimer(ros::Duration(1.0), &LightBuoyController::IncrementState, this);
 }
 
-void LightBuoyController::incrementState(const ros::TimerEvent& _event)
+void LightBuoyController::IncrementState(const ros::TimerEvent& _event)
 {
-  std::lock_guard<std::mutex> lock(mutex_);
+  (void) _event;
+  std::lock_guard<std::mutex> lock(this->mutex);
   // Start over if at end of pattern
-  if (state_ > 3)
-      state_ = 0;
-  auto msg = COLORS[pattern_[state_]].first;
+  if (this->state > 3)
+      this->state = 0;
+  auto msg = COLORS[this->pattern[this->state]].first;
   // Publish current color to each panel
   for (size_t i = 0; i < 3; ++i)
-    panel_pubs_[i].publish(msg);
+    this->panelPubs[i].publish(msg);
   // Increment index for next timer callback
-  ++state_;
+  ++this->state;
 }
 
-bool LightBuoyController::changePattern(std_srvs::Trigger::Request& req, std_srvs::Trigger::Response& res)
+bool LightBuoyController::ChangePattern(std_srvs::Trigger::Request& _req, std_srvs::Trigger::Response& _res)
 {
-  changePattern(res.message);
-  res.message = "New pattern: " + res.message;
-  res.success = true;
+  ChangePattern(_res.message);
+  _res.message = "New pattern: " + _res.message;
+  _res.success = true;
   return true;
 }
 
-void LightBuoyController::changePattern(std::string& message)
+void LightBuoyController::ChangePattern(std::string& _message)
 {
   pattern_t new_pattern;
   // Last phase in pattern is always off
   new_pattern[3] = 4;
+  // Loop until random pattern is different from current one
   do {
     // Generate random sequence of 3 colors among RED, GREEN, BLUE, and YELLOW
     for (size_t i = 0; i < 3; ++i)
@@ -91,16 +93,16 @@ void LightBuoyController::changePattern(std::string& message)
     // Ensure there is no CONSECUTIVE repeats
     while(new_pattern[0] == new_pattern[1] || new_pattern[1] == new_pattern[2])
         new_pattern[1] = ignition::math::Rand::IntUniform(0, 3);
-  } while(new_pattern == pattern_);
+  } while(new_pattern == this->pattern);
 
-  std::lock_guard<std::mutex> lock(mutex_);
+  std::lock_guard<std::mutex> lock(this->mutex);
   // Copy newly generated pattern to pattern
-  pattern_ = new_pattern;
+  this->pattern = new_pattern;
   // Start in OFF state so pattern restarts at beginning
-  state_ = 3;
+  this->state = 3;
   // Generate string representing pattern, ex: "RGB"
   for (size_t i = 0; i < 3; ++i)
-    message += COLORS[i].second[0];
+    _message += COLORS[new_pattern[i]].second[0];
   // Log the new pattern
-  ROS_INFO_NAMED("light_bouy_controller", "Pattern is %s", message.c_str());
+  ROS_INFO_NAMED("light_bouy_controller", "Pattern is %s", _message.c_str());
 }
