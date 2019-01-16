da8952241ba72d29dbe6339f9b4cdeb2fc5dead9
Carlos Aguero <devnull@localhost>
2019-01-15 16:41:45 -0800

    Crossing gates working.

--- vmrc_gazebo/include/vmrc_gazebo/navigation_scoring_plugin.hh
+++ vmrc_gazebo/include/vmrc_gazebo/navigation_scoring_plugin.hh
@@ -19,20 +19,62 @@
 #define VMRC_GAZEBO_NAVIGATION_SCORING_PLUGIN_HH_
 
 #include <ros/ros.h>
+#include <cstdint>
+#include <string>
+#include <vector>
 #include <gazebo/common/Events.hh>
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/math/Pose.hh>
+#include <gazebo/physics/Model.hh>
+#include <gazebo/physics/World.hh>
 #include <sdf/sdf.hh>
 
 /// \brief A plugin for computing the score of the navigation task.
 class NavigationScoringPlugin : public gazebo::WorldPlugin
 {
+  /// \brief A gate that is part of the navigation challenge.
+  class Gate
+  {
+    /// \brief Constructor.
+    /// \param[in] _redBuoyName The red buoy's model.
+    /// \param[in] _greenBuoyName The green buoy's model.
+    public: Gate(const gazebo::physics::ModelPtr _redBuoyModel,
+                 const gazebo::physics::ModelPtr _greenBuoyModel);
+
+    // Recalculate the pose and width of the gate.
+    public: void Update();
+
+    // The red buoy.
+    public: gazebo::physics::ModelPtr redBuoyModel;
+
+    // The green buoy.
+    public: gazebo::physics::ModelPtr greenBuoyModel;
+
+    // The center of the gate.
+    public: gazebo::math::Pose pose;
+
+    // The width of the gate.
+    public: double width;
+  };
+
   // Constructor.
   public: NavigationScoringPlugin();
 
   // Documentation inherited.
   public: void Load(gazebo::physics::WorldPtr _world,
                     sdf::ElementPtr _sdf);
+
+  /// \brief Parse the gates from SDF.
+  /// \param[in] _sdf The current SDF element.
+  /// \return True when the gates were successfully parsed or false othwerwise.
+  private: bool ParseGates(sdf::ElementPtr _sdf);
+
+  /// \brief Register a new gate.
+  /// \param[in] _redBuoyName The name of the red buoy.
+  /// \param[in] _greenBuoyName The name of the green buoy.
+  /// \return True when the gate has been registered or false otherwise.
+  private: bool AddGate(const std::string &_redBuoyName,
+                        const std::string &_greenBuoyName);
 
   /// \brief Callback executed at every world update.
   private: void Update();
@@ -43,20 +85,8 @@ class NavigationScoringPlugin : public gazebo::WorldPlugin
   /// \param _gateWorldPose Width of the gate
   /// \return If not "in" the gate, return 0; else return -1 if "before" the
   ///         gate, 1 if "after" the gate.
-  private: int IsPoseInGate(const gazebo::math::Pose &_robotWorldPose,
-                            const gazebo::math::Pose &_gateWorldPose,
-                            double _gateWidth);
-
-  /// \brief A gate.
-  struct Gate
-  {
-    // The center of the gate.
-    gazebo::math::Pose pose;
-
-    // The width of the gate.
-    double width;
-  };
-
+  private: uint8_t IsPoseInGate(const gazebo::math::Pose &_robotWorldPose,
+                                const Gate &_gate) const;
   /// \brief All the gates.
   private: std::vector<Gate> gates;
 
@@ -67,6 +97,10 @@ class NavigationScoringPlugin : public gazebo::WorldPlugin
   private: gazebo::physics::WorldPtr world;
 
   /// \brief Gate states.
+  ///  0: Not "in" the gate.
+  /// -1: Before the gate.
+  ///  1: After the gate.
+  ///  2: Gate crossed!
   private: std::vector<int8_t> gateStates;
 };
 
--- vmrc_gazebo/src/navigation_scoring_plugin.cc
+++ vmrc_gazebo/src/navigation_scoring_plugin.cc
@@ -15,11 +15,50 @@
  *
 */
 
-#include <gazebo/physics/Model.hh>
-#include <gazebo/physics/World.hh>
+#include <cmath>
+#include <gazebo/common/Assert.hh>
+#include <gazebo/common/Console.hh>
 #include "vmrc_gazebo/navigation_scoring_plugin.hh"
 
-using namespace gazebo;
+/////////////////////////////////////////////////
+NavigationScoringPlugin::Gate::Gate(
+    const gazebo::physics::ModelPtr _redBuoyModel,
+    const gazebo::physics::ModelPtr _greenBuoyModel)
+  : redBuoyModel(_redBuoyModel),
+    greenBuoyModel(_greenBuoyModel)
+{
+  this->Update();
+}
+
+/////////////////////////////////////////////////
+void NavigationScoringPlugin::Gate::Update()
+{
+  if (!this->redBuoyModel || !this->greenBuoyModel)
+    return;
+
+  // The pose of the buoys delimiting the gate.
+  auto redPose = this->redBuoyModel->GetWorldPose();
+  auto greenPose = this->greenBuoyModel->GetWorldPose();
+
+  // Unit vector from the green buoy to the red one.
+  auto v1 = redPose.pos - greenPose.pos;
+  v1.Normalize();
+
+  // Unit vector perpendicular to v1 in the direction we like to cross gates.
+  auto v2 = gazebo::math::Vector3::UnitZ.Cross(v1);
+
+  // This is the center point of the gate.
+  auto middle = (redPose.pos + greenPose.pos) / 2.0;
+
+  // Yaw of the gate in world coordinates.
+  auto yaw = atan2(v2.y, v2.x);
+
+  // The updated pose.
+  this->pose.Set(middle, gazebo::math::Vector3(0, 0, yaw));
+
+  // The updated width.
+  this->width = redPose.pos.Distance(greenPose.pos);
+}
 
 /////////////////////////////////////////////////
 NavigationScoringPlugin::NavigationScoringPlugin()
@@ -31,8 +70,8 @@ NavigationScoringPlugin::NavigationScoringPlugin()
 void NavigationScoringPlugin::Load(gazebo::physics::WorldPtr _world,
     sdf::ElementPtr _sdf)
 {
-  GZ_ASSERT(_world, "CommsBrokerPlugin world pointer is NULL");
-  GZ_ASSERT(_sdf,   "CommsBrokerPlugin::Load() error: _sdf pointer is NULL");
+  GZ_ASSERT(_world, "NavigationScoringPlugin::Load(): NULL world pointer");
+  GZ_ASSERT(_sdf,   "NavigationScoringPlugin::Load(): NULL _sdf pointer");
 
   this->world = _world;
 
@@ -43,83 +82,93 @@ void NavigationScoringPlugin::Load(gazebo::physics::WorldPtr _world,
     return;
   }
 
+  // Parse all the gates.
   auto const &gatesElem = _sdf->GetElement("gates");
-
-  // We need at least one gate.
-  if (!gatesElem->HasElement("gate"))
+  if (!this->ParseGates(gatesElem))
   {
-    gzerr << "Unable to find any gate" << std::endl;
+    gzerr << "Score disabled" << std::endl;
     return;
   }
 
-  auto gateElem = gatesElem->GetElement("gate");
+  this->updateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(
+    std::bind(&NavigationScoringPlugin::Update, this));
+}
+
+//////////////////////////////////////////////////
+bool NavigationScoringPlugin::ParseGates(sdf::ElementPtr _sdf)
+{
+  GZ_ASSERT(_sdf, "NavigationScoringPlugin::ParseGates(): NULL _sdf pointer");
+
+  // We need at least one gate.
+  if (!_sdf->HasElement("gate"))
+  {
+    gzerr << "Unable to find any gate" << std::endl;
+    return false;
+  }
+
+  auto gateElem = _sdf->GetElement("gate");
 
   // Parse a new gate.
   while (gateElem)
   {
-    // The red buoy.
+    // The red buoy's name.
     if (!gateElem->HasElement("red"))
     {
       gzerr << "Unable to find red element" << std::endl;
-      return;
+      return false;
     }
 
-    std::string redModelName = gateElem->Get<std::string>("red");
-    gazebo::physics::ModelPtr redModel = _world->GetModel(redModelName);
+    std::string redBuoyName = gateElem->Get<std::string>("red");
 
-    // Sanity check: Make sure that the model exists.
-    if (!redModel)
-    {
-      gzerr << "Unable to find model [" << redModelName << "]" << std::endl;
-      return;
-    }
-
-    // The green buoy.
+    // The green buoy's name.
     if (!gateElem->HasElement("green"))
     {
       gzerr << "Unable to find green element" << std::endl;
-      return;
+      return false;
     }
 
-    std::string greenModelName = gateElem->Get<std::string>("green");
-    gazebo::physics::ModelPtr greenModel = _world->GetModel(greenModelName);
+    std::string greenBuoyName = gateElem->Get<std::string>("green");
 
-    // Sanity check: Make sure that the model exists.
-    if (!greenModel)
-    {
-      gzerr << "Unable to find model [" << greenModelName << "]" << std::endl;
-      return;
-    }
-
-    // Create a new gate and save it.
-    auto redPose = redModel->GetWorldPose();
-    auto greenPose = greenModel->GetWorldPose();
-    auto gatePose = redPose - greenPose;
-  
-    std::cout << "Red: " << redPose << std::endl;
-    std::cout << "Green: " << greenPose << std::endl;
-
-    auto v = redPose.pos - greenPose.pos;
-    v.Normalize();
-    auto v2 = v.Cross(gazebo::math::Vector3::UnitZ);
-    auto middle = (redPose.pos + greenPose.pos) / 2.0;
-
-    auto yaw = atan2(v2.y, v2.x);
-
-    std::cout << "Gate: \n" << "  point: " << middle << "\n"
-              << "  vector: " << v2 << std::endl
-              << "  angle: " << yaw * 180 / 3.14159 << std::endl;
-
-    this->gates.push_back({{middle.x, middle.y, middle.z, 0, 0, yaw},
-                           redPose.pos.Distance(greenPose.pos)});
-    this->gateStates.push_back(0);
+    if (!this->AddGate(redBuoyName, greenBuoyName))
+      return false;
 
     // Parse the next gate.
     gateElem = gateElem->GetNextElement("gate");
   }
 
-  this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-    std::bind(&NavigationScoringPlugin::Update, this));
+  return true;
+}
+
+//////////////////////////////////////////////////
+bool NavigationScoringPlugin::AddGate(const std::string &_redBuoyName,
+    const std::string &_greenBuoyName)
+{
+  gazebo::physics::ModelPtr redBuoyModel = this->world->GetModel(_redBuoyName);
+
+  // Sanity check: Make sure that the model exists.
+  if (!redBuoyModel)
+  {
+    gzerr << "Unable to find model [" << _redBuoyName << "]" << std::endl;
+    return false;
+  }
+
+  gazebo::physics::ModelPtr greenBuoyModel =
+    this->world->GetModel(_greenBuoyName);
+
+  // Sanity check: Make sure that the model exists.
+  if (!greenBuoyModel)
+  {
+    gzerr << "Unable to find model [" << _greenBuoyName << "]" << std::endl;
+    return false;
+  }
+
+  // Save the new gate.
+  this->gates.push_back(Gate(redBuoyModel, greenBuoyModel));
+
+  // Initialize the state of the gate.
+  this->gateStates.push_back(0);
+
+  return true;
 }
 
 //////////////////////////////////////////////////
@@ -130,23 +179,43 @@ void NavigationScoringPlugin::Update()
     return;
 
   auto robotPose = robotModel->GetWorldPose();
-  auto gate = this->gates.front();
-  if (this->IsPoseInGate(robotPose, gate.pose, gate.width) == 1)
-    std::cout << "After 1st gate!" << std::endl;
+
+  // Update the state of all gates.
+  for (auto i = 0; i < this->gates.size(); ++i)
+  {
+    // Ignore all gates that have been crossed.
+    if (this->gateStates[i] == 2)
+      continue;
+
+    // Get the next gate.
+    auto &gate = this->gates[i];
+
+    // Update this gate (in case it moved).
+    gate.Update();
+
+    // Check if we have crossed this gate.
+    auto currentState = this->IsPoseInGate(robotPose, gate);
+    if (currentState == 1 && gateStates[i] == -1)
+    {
+      currentState = 2;
+      gzmsg << "Gate " << i << " crossed!" << std::endl;
+    }
+
+    gateStates[i] = currentState;
+  }
 }
 
 /////////////////////////////////////////////////
-int NavigationScoringPlugin::IsPoseInGate(
-    const gazebo::math::Pose &_robotWorldPose,
-    const gazebo::math::Pose &_gateWorldPose, double _gateWidth)
+uint8_t NavigationScoringPlugin::IsPoseInGate(
+    const gazebo::math::Pose &_robotWorldPose, const Gate &_gate) const
 {
   // Transform to gate frame.
   gazebo::math::Vector3 robotLocalPosition =
-    _gateWorldPose.rot.GetInverse().RotateVector(_robotWorldPose.pos -
-    _gateWorldPose.pos);
+    _gate.pose.rot.GetInverse().RotateVector(_robotWorldPose.pos -
+    _gate.pose.pos);
 
   // Are we within the width?
-  if (fabs(robotLocalPosition.y) <= _gateWidth / 2.0)
+  if (fabs(robotLocalPosition.y) <= _gate.width / 2.0)
     return (robotLocalPosition.x >= 0.0) ? 1 : -1;
   else
     return 0;
