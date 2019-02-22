b4dcb714b3b89e1ef4f09e890961bb371b012cce
m1chaelm <devnull@localhost>
2019-02-21 21:22:10 -0800

    skeleton of wayfinding_scoring_plugin.cc

---
+++ vmrc_gazebo/src/wayfinding_scoring_plugin.cc
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <cmath>
+#include <gazebo/common/Assert.hh>
+#include <gazebo/common/Console.hh>
+#include <std_msgs/Float64.h>
+#include "std_msgs/String.h"
+#include <sstream>
+#include <ignition/math/Pose3.hh>
+#include <ignition/math/Quaternion.hh>
+#include <gazebo/common/common.hh>
+#include "vmrc_gazebo/stationkeeping_scoring_plugin.hh"
+#include "gazebo/common/SphericalCoordinates.hh"
+
+
+/////////////////////////////////////////////////
+StationkeepingScoringPlugin::StationkeepingScoringPlugin()
+{
+  gzmsg << "Stationkeeping scoring plugin loaded" << std::endl;
+}
+
+/////////////////////////////////////////////////
+void StationkeepingScoringPlugin::Load(gazebo::physics::WorldPtr _world,
+    sdf::ElementPtr _sdf)
+{
+  ScoringPlugin::Load(_world, _sdf);
+  this->sdf = _sdf;
+  gzmsg << "Task [" << this->TaskName() << "]" << std::endl;
+
+
+  // A waypoints element is required. 
+  if (!this->sdf->HasElement("waypoints"))
+  {
+	  gzerr << "Unable to find <waypoints> element in SDF." << std::endl;
+          return false;
+  }
+  auto waypointsElem = this->sdf->GetElement("waypoints");
+
+  // We need at least one waypoint
+  if (!this->sdf->HasElement("waypoint"))
+  {
+      gzerr << "Unable to find <waypoint> element in SDF." << std::endl;
+      return false;
+  }
+  auto waypointElem = this->sdf->GetElement("waypoint");
+
+  while (waypointElem)
+  {
+    
+    this->sphericalWaypoints.push_back(this->sdf->Get<ignition::math::Vector3d>("waypoint"));
+
+    waypointElem = waypointElem->GetNextElement("waypoint"); 
+  }
+
+  
+}
+
+//////////////////////////////////////////////////
+void StationkeepingScoringPlugin::Update()
+{
+  // The vehicle might not be ready yet, let's try to get it.
+  if (!this->vehicleModel)
+  {
+    this->vehicleModel = this->world->GetModel(this->vehicleName);
+    if (!this->vehicleModel)
+      return;
+  }
+
+}
+
+
+//////////////////////////////////////////////////
+void StationkeepingScoringPlugin::PublishGoal()
+{
+  gzmsg << "Publishing Goal coordinates" << std::endl;
+}
+
+//////////////////////////////////////////////////
+void StationkeepingScoringPlugin::OnReady()
+{
+  gzmsg << "OnReady" << std::endl;
+
+  this->PublishGoal();
+}
+
+
+//////////////////////////////////////////////////
+void StationkeepingScoringPlugin::OnRunning()
+{
+  gzmsg << "OnRunning" << std::endl;
+
+}
+
+//////////////////////////////////////////////////
+void StationkeepingScoringPlugin::OnFinished()
+{
+  gzmsg << "OnFinished" << std::endl;
+}
+
+// Register plugin with gazebo
+GZ_REGISTER_WORLD_PLUGIN(StationkeepingScoringPlugin)
