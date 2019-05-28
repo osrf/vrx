26607ce8b55a50464c642246b9224eee9225af64
Rumman Waqar <rumman.waqar05@gmail.com>
2019-05-28 14:13:15 -0600

    Setup buoyancy test build system + test world

---
+++ usv_gazebo_plugins/test/buoyancy_test.cc
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <gazebo/test/ServerFixture.hh>
+
+using namespace gazebo;
+
+class BuoyancyPluginTest : public ServerFixture {
+public:
+  BuoyancyPluginTest() {
+    Load("worlds/buoyancy_plugin_test.world", true);
+  }
+};
+
+/////////////////////////////////////////////////
+TEST_F(BuoyancyPluginTest, FloatingBox) {
+  physics::WorldPtr world = physics::get_world("buoyancy_test");
+  ASSERT_TRUE(world != nullptr);
+
+  physics::ModelPtr model = world->ModelByName("1_box");
+  ASSERT_TRUE(model != nullptr);
+}
+
+/////////////////////////////////////////////////
+/// Main
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
