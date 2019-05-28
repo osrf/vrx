444c9314a26a07e0459f0487d6c2ab341ae8d203
MarshallRawson <devnull@localhost>
2019-05-28 10:51:19 -0700

    incremental, now supports macros with no parameters

--- vrx_gazebo/scripts/generator_scripts/sensor_config/configure_sensors.py
+++ vrx_gazebo/scripts/generator_scripts/sensor_config/configure_sensors.py
@@ -7,6 +7,6 @@ macro_block_gen(availible='../../wamv_gazebo/urdf/sensors',#directory containing
                 requested='generator_scripts/sensor_config/sensor_config.yaml',#yaml requesting which sensors with what parameters go where 
                 target='../../wamv_description/urdf/sensors.xacro',#where the macro block is going
                 boiler_plate_top='<?xml version="1.0"?>\n<robot xmlns:xacro="http://ros.org/wiki/xacro" name="wam-v-sensors">\n<xacro:macro name="sensors">\n',#things to start/open the macro
-                boiler_plate_bot = '</xacro:macro>\n</robot>'#things to close the macro
+                boiler_plate_bot = '</xacro:macro>\n</robot>',#things to close the macro
                 num_test = sensor_number_compliance,#function to test the number of a certain type of sensor is in compliance
                 param_test = param_compliance)#function to test if a given instance of a sensor is in compliance
