35639bb8e1e7fb6f4c772464097a3741d617771a
MarshallRawson <devnull@localhost>
2019-05-31 10:48:55 -0700

    Added support for sequence breakout specified in yaml file

--- vrx_gazebo/src/vrx_gazebo_python/generator_scripts/world_generator/generate_worlds.py
+++ vrx_gazebo/src/vrx_gazebo_python/generator_scripts/world_generator/generate_worlds.py
@@ -12,32 +12,33 @@ def main():
     s = open(rospy.get_param('requested'))
     master = yaml.load(s)
     
-    a = linear_combinations(master)
-    print len(a)
+    coordinates = linear_combinations(master)
+
+    for num,i in enumerate(coordinates):
+        macro_block_gen(target = rospy.get_param('world_xacro_target') + 'world' + str(num) + '.world.xacro',
+                        available = rospy.get_param('available'),
+                        requested_macros = world_gen(coordinate=i, master=master),
+                        boiler_plate_top = '<?xml version="1.0" ?>\n<sdf version="1.6" xmlns:xacro="http://ros.org/wiki/xacro">\n<world name="robotx_example_course">\n<xacro:include filename="$(find vrx_gazebo)/worlds/xacros/include_all_xacros.xacro" />\n',
+                        boiler_plate_bot = '</world>\n</sdf>')
+        os.system('rosrun xacro xacro --inorder -o' +
+                  rospy.get_param('world_target') + 'world' + str(num) + '.world ' +
+                  rospy.get_param('world_xacro_target')+ 'world' + str(num) + '.world.xacro')
     
-    '''
-    a = world_gen(coordinates={'environment':0}, master=master)
-    macro_block_gen(target = rospy.get_param('world_xacro_target') + 'world1.world.xacro',
-                    available = rospy.get_param('available'),
-                    requested_macros = a,
-                    boiler_plate_top = '<?xml version="1.0" ?>\n<sdf version="1.6" xmlns:xacro="http://ros.org/wiki/xacro">\n<world name="robotx_example_course">\n<xacro:include filename="$(find vrx_gazebo)/worlds/sandisland.xacro" />\n',
-                    boiler_plate_bot = '</world>\n</sdf>')
-    os.system('rosrun xacro xacro --inorder -o' +
-              rospy.get_param('world_target') + 'world1.world ' +
-              rospy.get_param('world_xacro_target')+ 'world1.world.xacro')
-    '''
 
 
 
-def world_gen(coordinates = {}, master = {}):
+def world_gen(coordinate = {}, master = {}):
     world = {}
     for axis_name, axis in master.iteritems():
-        for macro_name, macros in axis['macros'].iteritems():
+        if coordinate[axis_name] in axis['sequence']:
+            world.update(axis['sequence'][coordinate[axis_name]])
+            break
+        for macro_name, macro_calls in axis['macros'].iteritems():
             world[macro_name] = []
-            for params in macros:
+            for params in macro_calls:
                 if params != None:
                     for param, value in params.iteritems():
-                        params[param] = (lambda n: eval(value))(coordinates[axis_name])
+                        params[param] = (lambda n: eval(value))(coordinate[axis_name])
                     world[macro_name].append(params)
                 else:
                     world[macro_name].append({})
