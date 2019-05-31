f0e6f28fd8d3344945e963b86a4b4cc514c6ed94
MarshallRawson <devnull@localhost>
2019-05-31 11:37:25 -0700

    increased flexibility of compliance.py

--- vrx_gazebo/src/vrx_gazebo_python/generator_scripts/sensor_config/compliance.py
+++ vrx_gazebo/src/vrx_gazebo_python/generator_scripts/sensor_config/compliance.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
-def param_compliance(sensor_type, params={}):
-    # an instance of 'wamv_camera' with params, is this camera in compliance
-    return True
+class Compliance:
+    def __init__(self):
+        return
+    def param_compliance(self, sensor_type, params={}):
+        # an instance of 'wamv_camera' with params, is this camera in compliance
+        return True
 
 
-def sensor_number_compliance(sensor_type, n):
-    # ie: are n wamv_cameras allowed?
-    return True
+    def sensor_number_compliance(self, sensor_type, n):
+        # ie: are n wamv_cameras allowed?
+        return True
