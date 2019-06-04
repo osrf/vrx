a9232266d815cc45136608daf7434cb67467cb04
MarshallRawson <devnull@localhost>
2019-06-04 11:04:34 -0700

    added more comments

--- vrx_gazebo/src/vrx_gazebo_python/generator_scripts/world_generator/generate_worlds.py
+++ vrx_gazebo/src/vrx_gazebo_python/generator_scripts/world_generator/generate_worlds.py
@@ -4,74 +4,91 @@ import rospy
 import os
 from collections import OrderedDict
 
-
 from .. utils import macro_block_gen
 
+
 def main():
-
     s = open(rospy.get_param('requested'))
+    # get the yaml as a dict
     master = yaml.load(s)
-    
+    # get lsit of all coordinates that the master dict maps out
     coordinates = linear_combinations(master)
-
-    for num,i in enumerate(coordinates):
-        macro_block_gen(target = rospy.get_param('world_xacro_target') + 'world' + str(num) + '.world.xacro',
-                        requested_macros = world_gen(coordinate=i, master=master),
-                        boiler_plate_top = '<?xml version="1.0" ?>\n<sdf version="1.6" xmlns:xacro="http://ros.org/wiki/xacro">\n<world name="robotx_example_course">\n  <xacro:include filename="$(find vrx_gazebo)/worlds/xacros/include_all_xacros.xacro" />\n  <xacro:include_all_xacros />\n',
-                        boiler_plate_bot = '</world>\n</sdf>')
+    # create a world xacro and subsiquent world file for each coordinate
+    for num, i in enumerate(coordinates):
+        macro_block_gen(target=rospy.get_param('world_xacro_target') +
+                        'world' + str(num) + '.world.xacro',
+                        requested_macros=world_gen(coordinate=i,
+                                                   master=master),
+                        boiler_plate_top='<?xml version="1.0" ?>\n' +
+                        '<sdf version="1.6" ' +
+                        'xmlns:xacro="http://ros.org/wiki/xacro">\n' +
+                        '<world name="robotx_example_course">\n' +
+                        '  <xacro:include filename="$(find vrx_gazebo)' +
+                        '/worlds/xacros/include_all_xacros.xacro" />\n' +
+                        '  <xacro:include_all_xacros />\n',
+                        boiler_plate_bot='</world>\n</sdf>')
         os.system('rosrun xacro xacro --inorder -o' +
-                  rospy.get_param('world_target') + 'world' + str(num) + '.world ' +
-                  rospy.get_param('world_xacro_target')+ 'world' + str(num) + '.world.xacro')
-    
+                  rospy.get_param('world_target') + 'world' + str(num) +
+                  '.world ' +
+                  rospy.get_param('world_xacro_target') + 'world' +
+                  str(num) + '.world.xacro')
 
 
-
-def world_gen(coordinate = {}, master = {}):
+def world_gen(coordinate={}, master={}):
     world = {}
     for axis_name, axis in master.iteritems():
-        #if a sequence instance is explicitely defined
-        if axis['sequence'] != None and coordinate[axis_name] in axis['sequence']:
-            #concatonate all the repeate macros
+        # if a sequence override defined for this axis at this step
+        if axis['sequence'] is not None and \
+                coordinate[axis_name] in axis['sequence']:
+            # instances of macros already present in the world dict
             for i in world:
                 if i in axis['sequence'][coordinate[axis_name]]:
                     world[i] += axis['sequence'][coordinate[axis_name]]
-            #add all unique macros in sequence to world
+            # add all unique macros in sequence to world
             for i in axis['sequence'][coordinate[axis_name]]:
-                if not i in world:
+                if i not in world:
                     if axis['sequence'][coordinate[axis_name]][i] == [None]:
                         world[i] = [{}]
                     else:
                         world[i] = axis['sequence'][coordinate[axis_name]][i]
-
+        # for the non-sequence override case:
         else:
             for macro_name, macro_calls in axis['macros'].iteritems():
+                # if this one is new
                 if macro_name not in world:
                     world[macro_name] = []
                 for params in macro_calls:
-                    if params != None:
+                    if params is not None:
                         for param, value in params.iteritems():
+                            # name parameters will not be evaluated as lambdas
                             if str(param) == 'name':
                                 params[param] = str(value)
                             else:
-                                params[param] = (lambda n: eval(str(value)))(coordinate[axis_name])
+                                params[param] = (lambda n: eval(
+                                    str(value)))(coordinate[axis_name])
                         world[macro_name].append(params)
                     else:
                         world[macro_name].append({})
     return world
 
-def linear_combinations(master = {}):
+
+def linear_combinations(master={}):
     combinations = []
     axies = OrderedDict()
     start = {}
+    # make the starting spot and max
+    # NOTE:the start coordinate <= all coordinates on an axis <= axies max
     for axis, value in master.iteritems():
         start[axis] = 0
         axies[axis] = value['steps']-1
-    iterate(axies_max = axies, coordinates = combinations, current_coordinate = start)
+    iterate(axies_max=axies, coordinates=combinations,
+            current_coordinate=start)
     return combinations
 
 
-def iterate(axies_max={}, coordinates=[], current_coordinate = {}):
+def iterate(axies_max={}, coordinates=[], current_coordinate={}):
     coordinates.append(current_coordinate.copy())
+    # if we are at the max coordinate, return
     if axies_max == current_coordinate:
         return
     else:
@@ -81,6 +98,5 @@ def iterate(axies_max={}, coordinates=[], current_coordinate = {}):
                 break
             else:
                 current_coordinate[axis] = 0
-        iterate(axies_max=axies_max, coordinates = coordinates, current_coordinate = current_coordinate)
-
-
+        iterate(axies_max=axies_max, coordinates=coordinates,
+                current_coordinate=current_coordinate)
