8b8128241af678a664c043046f58a86672ae7f1c
Tyler Lum <tylergwlum@gmail.com>
2019-06-09 15:30:51 -0700

    Attempt to fix build issues SetAngle->SetPosition

--- usv_gazebo_plugins/src/usv_gazebo_thrust_plugin.cc
+++ usv_gazebo_plugins/src/usv_gazebo_thrust_plugin.cc
@@ -348,7 +348,7 @@ void UsvThrust::Update()
       #if GAZEBO_MAJOR_VERSION >= 8
         this->thrusters[i].engineJoint->SetPosition(0, this->thrusters[i].currAngle, true);
       #else
-        this->thrusters[i].engineJoint->SetAngle(0, gazebo::math::Angle(this->thrusters[i].currAngle));
+        this->thrusters[i].engineJoint->SetPosition(0, this->thrusters[i].currAngle);
       #endif
 
       // Apply the thrust mapping
