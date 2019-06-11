116dc46b085c72e9e2115c298451cbd8a119a4b4
Tyler Lum <tylergwlum@gmail.com>
2019-06-11 12:40:55 -0700

    Attempt to implement basic GUI overlay to show VRX Task Info. Stuck on build issues with Qt5

---
+++ vrx_gazebo/include/vrx_gazebo/gui_task_widget.hh
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2014 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef _GUI_TASK_WIDGET_HH_
+#define _GUI_TASK_WIDGET_HH_
+
+#include <string>
+
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/gui/GuiPlugin.hh>
+#include <gazebo/transport/transport.hh>
+
+namespace gazebo
+{
+  class GAZEBO_VISIBLE GUITaskWidget : public GUIPlugin
+  {
+    Q_OBJECT
+
+    /// \brief Constructor
+    public: GUITaskWidget();
+
+    /// \brief Destructor
+    public: virtual ~GUITaskWidget();
+
+    /// \brief A signal used to set the task info line edit.
+    /// \param[in] _string String representation of task info.
+    signals: void SetTaskInfo(QString _string);
+
+    /// \brief Callback that received task info messages.
+    /// \param[in] _msg Task info message that is received.
+    protected: void OnTaskInfo(ConstTaskPtr &_msg);
+
+    /// \brief Helper function to format time string.
+    /// \param[in] _msg Time message.
+    /// \return Time formatted as a string.
+    private: std::string FormatTime(const msgs::Time &_msg) const;
+
+    /// \brief Node used to establish communication with gzserver.
+    private: transport::NodePtr node;
+
+    /// \brief Subscriber to Task messages.
+    private: transport::SubscriberPtr taskSub;
+  };
+}
+
+#endif
