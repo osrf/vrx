93aae56faab1d15791859d16e475752babaa388a
MarshallRawson <devnull@localhost>
2019-06-11 13:20:46 -0700

    styling

--- vrx_gazebo/src/vrx_gazebo_python/generator_scripts/wamv_config/compliance.py
+++ vrx_gazebo/src/vrx_gazebo_python/generator_scripts/wamv_config/compliance.py
@@ -3,15 +3,21 @@ import os
 import numpy as np
 import yaml
 from .. import utils
+
+
 class Sensor_Compliance:
     def __init__(self):
         # open sensor_compliance_visual.sdf and all the boxes defined => boxes
         self.boxes = find_boxes('sensor_compliance_visual.sdf')
         # look at all sensors in sensors directory and get the default params
-        self.sensors_dir = os.getcwd()[:-4] + '/vrx_ws/src/vrx/wamv_gazebo/urdf/sensors/'
+        self.sensors_dir = os.getcwd()[:-4] +\
+            '/vrx_ws/src/vrx/wamv_gazebo/urdf/sensors/'
         self.default_parameters = utils.get_macros(self.sensors_dir)
-        self.dir = os.getcwd()[:-4] + 'vrx_ws/src/vrx/vrx_gazebo/src/vrx_gazebo_python/generator_scripts/wamv_config/'
-        self.numeric = yaml.load(open(self.dir + 'sensor_compliance_numeric.yaml'))
+        self.dir = os.getcwd()[:-4] +\
+            'vrx_ws/src/vrx/vrx_gazebo/src/vrx_gazebo_python/' +\
+            'generator_scripts/wamv_config/'
+        self.numeric = yaml.load(open(self.dir +
+                                      'sensor_compliance_numeric.yaml'))
         return
 
     def param_compliance(self, sensor_type, params={}):
@@ -24,7 +30,8 @@ class Sensor_Compliance:
         for i in self.default_parameters[sensor_type]:
             if i not in params:
                 params[i] = self.default_parameters[sensor_type][i]
-        #right now the ONLY compliance we have is to make sure that the sensors are in atleast one of the boxes
+        # right now the ONLY compliance check we have is to make sure that
+        # the sensors are in at least one of the boxes
         if 'x' and 'y' and 'z' in params:
             xyz = np.array([float(params['x']),
                             float(params['y']),
@@ -40,7 +47,8 @@ class Sensor_Compliance:
     def number_compliance(self, sensor_type, n):
         # ie: are n wamv_cameras allowed?
         if n > self.numeric[sensor_type]:
-            print '\n', 'maximum of', self.numeric[sensor_type], sensor_type, 'allowed\n'
+            print '\n', 'maximum of', self.numeric[sensor_type], sensor_type, \
+                'allowed\n'
             return False
         return True
 
@@ -61,9 +69,12 @@ class Thruster_Compliance:
 
 class Box:
     def __init__(self, pose, size):
-        self.pose = np.array([float(j) for j in [i for i in pose.split(' ') if i != '']])
-        self.size = np.array([float(j) for j in [i for i in size.split(' ') if i != '']])
+        self.pose = np.array([float(j) for j in [i for i in pose.split(' ')
+                              if i != '']])
+        self.size = np.array([float(j) for j in [i for i in size.split(' ')
+                              if i != '']])
         return
+
     def fit(self, pose):
         pose = pose-self.pose[:3]
         for idx, i in enumerate(pose):
@@ -73,9 +84,11 @@ class Box:
 
 
 def find_boxes(relative_addrs_of_sdf):
-    addrs = os.getcwd()[:-4] + 'vrx_ws/src/vrx/vrx_gazebo/src/vrx_gazebo_python/generator_scripts/wamv_config/' + relative_addrs_of_sdf
+    addrs = os.getcwd()[:-4] +\
+        'vrx_ws/src/vrx/vrx_gazebo/src/vrx_gazebo_python/generator_scripts/' +\
+        'wamv_config/' + relative_addrs_of_sdf
     sdf = open(addrs, 'r')
-    sdf = sdf.read() 
+    sdf = sdf.read()
     boxes = []
     while sdf.find('<visual') != -1:
         start = sdf.find('<visual')
@@ -89,17 +102,15 @@ def find_boxes(relative_addrs_of_sdf):
         end = sdf.find('<')
         pose = sdf[:end]
         sdf = sdf[end:]
-        
+
         start = sdf.find('<geometry')
         sdf = sdf[start:]
-        
+
         start = sdf.find('<box>')
         sdf = sdf[start:]
 
         start = sdf.find('<size>')
-        end = sdf.find('</') 
+        end = sdf.find('</')
         size = sdf[start+6:end]
         boxes.append(Box(pose, size))
     return boxes
-
-
