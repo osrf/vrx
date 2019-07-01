4ca1a76a33f082cce165c1315c19176e4deaf919
m1chaelm <devnull@localhost>
2019-06-30 21:51:48 -0700

    now interpreting time of spawned objects relative to start of running state

--- vrx_gazebo/src/perception_scoring_plugin.cc
+++ vrx_gazebo/src/perception_scoring_plugin.cc
@@ -414,15 +414,8 @@ void PerceptionScoringPlugin::OnUpdate()
   }
 
   // Check whether move the next object in the list.
-  #if GAZEBO_MAJOR_VERSION >= 8
-    auto elapsedTime = this->dataPtr->world->SimTime() -
-                       this->dataPtr->lastUpdateTime;
-  #else
-    auto elapsedTime = this->dataPtr->world->GetSimTime() -
-                       this->dataPtr->lastUpdateTime;
-  #endif
 
-  if (elapsedTime >= this->dataPtr->objects.front().time)
+  if (this->ElapsedTime() >= this->dataPtr->objects.front().time)
   {
     gzmsg << "PerceptionScoringPlugin: spawn next object." << std::endl;
     auto obj = this->dataPtr->objects.front();
