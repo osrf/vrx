8c814622830a7308947b9bad91565a1dfb70bcd3
Tyler Lum <tylergwlum@gmail.com>
2019-08-06 12:13:15 -0700

    Add wait for gzserver, might need testing

--- vrx_gazebo/scripts/spawn_wamv.bash
+++ vrx_gazebo/scripts/spawn_wamv.bash
@@ -12,6 +12,29 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NOCOLOR='\033[0m'
 
+is_gzserver_running()
+{
+  if pgrep gzserver >/dev/null; then
+    true
+  else
+    false
+  fi
+}
+
+wait_until_gzserver_is_up()
+{
+  until is_gzserver_running
+  do
+    sleep 2s
+  done
+
+  while [[ "$(gz topic -l | wc -l)" -le 2 ]];do
+    sleep 2s
+  done
+
+  sleep 2s
+}
+
 # Define usage function.
 usage()
 {
@@ -24,6 +47,6 @@ echo "*******************************#$##"
 echo "Has $# parameters"
 # [[ $# -ne 1 ]] && usage
 
-sleep 20s
-echo "DONE SLEEP"
+wait_until_gzserver_is_up
+echo "gzserver is up"
 gz model --model-name=wamv --spawn-file=$HOME/my_urdf.urdf
