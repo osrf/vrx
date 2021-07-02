fa0711c5b19139d5154d9c06c1320a1368309933
M1chaelM <mrmccarr@nps.edu>
2021-07-02 14:53:15 -0700

    updated simplify run.bash to user rocker

--- docker/run.bash
+++ docker/run.bash
@@ -15,93 +15,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#
 
 # Runs a docker container with the image created by build.bash
 # Requires:
 #   docker
 #   nvidia-docker
 #   an X server
+#   rocker
 # Recommended:
 #   A joystick mounted to /dev/input/js0 or /dev/input/js1
 
-RUNTIME="runc"
+IMG_NAME=$1
+JOY=/dev/input/js0
+CONTAINER_NAME=vrx_noetic_runtime
 
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
+rocker --devices $JOY --dev-helpers --nvidia --x11 --user --home --name $CONTAINER_NAME --git vrx_noetic:latest
 
-case $key in
-    -n|--nvidia)
-    RUNTIME="nvidia"
-    shift
-    ;;
-    *)    # unknown option
-    POSITIONAL+=("$1")
-    shift
-    ;;
-esac
-done
-
-set -- "${POSITIONAL[@]}"
-
-if [ $# -lt 1 ]
-then
-    echo "Usage: $0 [-n --nvidia] <docker image> [<dir with workspace> ...]"
-    exit 1
-fi
-
-
-
-IMG=$1
-
-ARGS=("$@")
-WORKSPACES=("${ARGS[@]:1}")
-
-# Make sure processes in the container can connect to the x server
-# Necessary so gazebo can create a context for OpenGL rendering (even headless)
-XAUTH=/tmp/.docker.xauth
-if [ ! -f $XAUTH ]
-then
-    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
-    if [ ! -z "$xauth_list" ]
-    then
-        echo $xauth_list | xauth -f $XAUTH nmerge -
-    else
-        touch $XAUTH
-    fi
-    chmod a+r $XAUTH
-fi
-
-VRX_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )"
-
-DOCKER_OPTS=
-# Example: Bind mount a local repository on the host machine:
-#DOCKER_OPTS="--mount type=bind,source=${VRX_PATH},target=/home/developer/vrx_ws/src/vrx"
-
-
-# Share your vim settings.
-# VIMRC=~/.vimrc
-# if [ -f $VIMRC ]
-# then
-#   DOCKER_OPTS="$DOCKER_OPTS -v $VIMRC:/home/developer/.vimrc:ro"
-# fi
-
-USERID=$(id -u)
-GROUPID=$(id -g)
-docker run -it \
-  -e DISPLAY \
-  -e QT_X11_NO_MITSHM=1 \
-  -e XAUTHORITY=$XAUTH \
-  -v "$XAUTH:$XAUTH" \
-  -v "/tmp/.X11-unix:/tmp/.X11-unix" \
-  -v "/etc/localtime:/etc/localtime:ro" \
-  -v "/dev/input:/dev/input" \
-  --privileged \
-  --rm \
-  --runtime=$RUNTIME \
-  --security-opt seccomp=unconfined \
-  -u $USERID:$GROUPID \
-  $DOCKER_OPTS \
-  $IMG
