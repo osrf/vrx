aa5bfe1acf7ba0ffc95f2f1d265fe943667283a8
Jessica Herman <jessica.herman@nps.edu>
2022-08-18 10:10:05 -0700

    Working minimal ScoringPlugin example

---
+++ vrx_ign/src/ScoringPlugin.cc
@@ -0,0 +1,409 @@
+#include <ignition/common/Console.hh>
+#include <ignition/physics/Joint.hh>
+#include <ignition/plugin/Register.hh>
+#include <ignition/gazebo/components/World.hh>
+#include <ignition/gazebo/EntityComponentManager.hh>
+#include <ignition/gazebo/Events.hh>
+#include <ignition/gazebo/SdfEntityCreator.hh>
+#include "ScoringPlugin.hh"
+
+void cb(const ignition::msgs::Contacts &_contacts){return;}
+
+ScoringPlugin::ScoringPlugin()
+
+{
+}
+
+void ScoringPlugin::Configure(const ignition::gazebo::Entity &_entity,
+                           const std::shared_ptr<const sdf::Element> &_sdf,
+                           ignition::gazebo::EntityComponentManager &_ecm,
+                           ignition::gazebo::EventManager &_eventMgr)
+{
+
+    this->creator = std::make_unique<ignition::gazebo::SdfEntityCreator>(_ecm, _eventMgr);
+    this->worldEntity = _ecm.EntityByComponents(ignition::gazebo::components::World());
+    this->eventManager = &_eventMgr;
+    this->sdf = _sdf;
+    //this->forceReturn = true; // Turns off update function!  Remove before use.
+    // SDF.
+    if (!this->ParseSDFParameters())
+    {
+        ignerr << "Scoring disabled" << std::endl;
+
+        return;
+    }
+
+    this->readyTime = std::chrono::duration<double>(this->initialStateDuration);
+    this->runningTime = this->readyTime +  std::chrono::duration<double>(this->readyStateDuration);
+    this->finishTime = this->runningTime + std::chrono::duration<double>(this->runningStateDuration);
+
+    // Prepopulate the task msg.
+
+    taskMessage.set_data(this->taskState);
+
+    this->taskPub = this->node.Advertise<ignition::msgs::StringMsg>(this->taskInfoTopic);
+    this->contactPub = this->node.Advertise<ignition::msgs::Contact>(this->contactDebugTopic);
+
+    std::string worldName = "sydney_regatta"; //hard code for testing
+    
+    std::string collisionTopic =
+        std::string("/gazebosim/") + worldName + std::string("/physics/contacts");
+             if (!gzNode->Subscribe(collisionTopic, &ScoringPlugin::OnCollisionMsg, this))
+    {
+        std::cerr << "Error subscribing to [" << collisionTopic << "]" << std::endl;
+        ignerr << "Error subscribing to [" << collisionTopic << "]" << std::endl;
+    }
+
+    this->serverControlPub = std::make_unique<ignition::transport::Node::Publisher>
+        (gzNode->Advertise<ignition::msgs::ServerControl>
+        ("/gazebosim/server/control")); */
+}
+
+//////////////////////////////////////////////////
+double ScoringPlugin::Score() const
+{
+  return this->score;
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::SetScore(double _newScore)
+{
+  if (this->TaskState() == "running")
+    this->score = _newScore;
+}
+
+//////////////////////////////////////////////////
+std::string ScoringPlugin::TaskName() const
+{
+  return this->taskName;
+}
+
+//////////////////////////////////////////////////
+std::string ScoringPlugin::TaskState() const
+{
+  return this->taskState;
+}
+
+//////////////////////////////////////////////////
+std::chrono::duration<double> ScoringPlugin::ElapsedTime() const
+{
+  return this->elapsedTime;
+}
+
+//////////////////////////////////////////////////
+std::chrono::duration<double> ScoringPlugin::RemainingTime() const
+{
+  return this->remainingTime;
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::Finish()
+{
+  if (this->taskState == "finished")
+    return;
+
+  this->taskState = "finished";
+  this->OnFinished();
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::PostUpdate(const ignition::gazebo::UpdateInfo &_info,
+                const ignition::gazebo::EntityComponentManager &_ecm)
+{
+    // Placeholder for testing
+    if (this->forceReturn)
+    {
+      return;
+    }
+
+    this->simTime = _info.simTime;
+    this->UpdateTime(simTime);
+    this->UpdateTaskState();
+    this->PublishStats();
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::UpdateTime(const std::chrono::duration<double> _simTime)
+{
+    this->currentTime = _simTime;
+    this->elapsedTime = this->currentTime - this->runningTime;
+    this->remainingTime = this->finishTime - this->currentTime;
+    this->timedOut = this->remainingTime <= std::chrono::duration<double>(0.0);
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::UpdateTaskState()
+{
+  if (this->taskState == "initial" &&
+      this->currentTime >= this->readyTime)
+  {
+    this->taskState = "ready";
+    this->ReleaseVehicle();
+    this->OnReady();
+    return;
+  }
+
+  if (this->taskState == "ready" &&
+      this->currentTime >= this->runningTime)
+  {
+    this->taskState = "running";
+    this->OnRunning();
+    return;
+  }
+
+  if (this->taskState == "running" && this->timedOut)
+  {
+    this->taskState = "finished";
+    this->OnFinished();
+    return;
+  }
+}
+
+//////////////////////////////////////////////////
+
+void ScoringPlugin::UpdateTaskMessage() //My simplified version
+{
+    this->taskMessage.set_data(this->taskState);
+}
+//////////////////////////////////////////////////
+void ScoringPlugin::PublishStats()
+{
+  this->UpdateTaskMessage();
+
+  // We publish stats at 1Hz.
+  if (this->currentTime - this->lastStatsSent >= std::chrono::duration<double>(1.0))
+  {
+    this->taskPub.Publish(this->taskMessage);
+    this->lastStatsSent = this->currentTime;
+  }
+}
+//////////////////////////////////////////////////
+void ScoringPlugin::ReleaseVehicle()
+{
+    ignmsg << "Scoring Plugin Released WAMV" << std::endl;
+}
+
+void ScoringPlugin::OnReady()
+{
+  if (!this->silent)
+    ignmsg << "ScoringPlugin::OnReady" << std::endl;
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::OnRunning()
+{
+  if (!this->silent)
+    ignmsg << "ScoringPlugin::OnRunning" << std::endl;
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::OnFinished()
+{
+  if (!this->silent)
+    ignmsg << this->simTime.count() << "  OnFinished" << std::endl;
+
+  // If a timeoutScore was specified, use it.
+  if (this->timedOut && this->timeoutScore > 0.0)
+  {
+    this->score = this->timeoutScore;
+  }
+  this->UpdateTaskMessage();
+  this->taskPub.Publish(this->taskMessage);
+  this->Exit();
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::OnCollision()
+{
+  ++this->numCollisions;
+}
+
+
+//////////////////////////////////////////////////
+void ScoringPlugin::OnCollisionMsg(const ignition::msgs::Contacts &_contacts)
+{
+    // loop through collisions, if any include the wamv, increment collision
+    // counter
+  ignmsg << "Collision Message" << std::endl;
+
+}
+
+//////////////////////////////////////////////////
+bool ScoringPlugin::ParseSDFParameters()
+{
+  // This is a required element.
+  if (!this->sdf->HasElement("vehicle"))
+  {
+    ignerr << "Unable to find <vehicle> element in SDF." << std::endl;
+    return false;
+  }
+  this->vehicleName = this->sdf->Get<std::string>("vehicle");
+
+  // This is a required element.
+  if (!this->sdf->HasElement("task_name"))
+  {
+    ignerr << "Unable to find <task_name> element in SDF." << std::endl;
+    return false;
+  }
+  this->taskName = this->sdf->Get<std::string>("task_name");
+
+  // This is an optional element.
+  if (this->sdf->HasElement("task_info_topic"))
+    this->taskInfoTopic = this->sdf->Get<std::string>("task_info_topic");
+
+  // This is an optional element.
+  if (this->sdf->HasElement("contact_debug_topic"))
+    this->contactDebugTopic = this->sdf->Get<std::string>
+      ("contact_debug_topic");
+
+  // This is an optional element.
+  if (this->sdf->HasElement("per_plugin_exit_on_completion"))
+    this->perPluginExitOnCompletion = this->sdf->Get<bool>(
+      "per_plugin_exit_on_completion");
+
+  // This is an optional element.
+  if (this->sdf->HasElement("initial_state_duration"))
+  {
+    double value = this->sdf->Get<double>("initial_state_duration");
+    if (value < 0)
+    {
+      ignerr << "<initial_state_duration> value should not be negative."
+            << std::endl;
+      return false;
+    }
+    this->initialStateDuration = value;
+  }
+
+  // This is an optional element.
+  if (this->sdf->HasElement("ready_state_duration"))
+  {
+    double value = this->sdf->Get<double>("ready_state_duration");
+    if (value < 0)
+    {
+      ignerr << "<ready_state_duration> value should not be negative."
+            << std::endl;
+      return false;
+    }
+
+    this->readyStateDuration = value;
+  }
+
+  // This is an optional element.
+  if (this->sdf->HasElement("running_state_duration"))
+  {
+    double value = this->sdf->Get<double>("running_state_duration");
+    if (value < 0)
+    {
+      ignerr << "<running_state_duration> value should not be negative."
+            << std::endl;
+      return false;
+    }
+    this->runningStateDuration = value;
+  }
+
+  // This is an optional element.
+  if (this->sdf->HasElement("collision_buffer"))
+  {
+    this->collisionBuffer = this->sdf->Get<float>("collision_buffer");
+  }
+
+  // This is an optional element.
+  if (this->sdf->HasElement("silent"))
+  {
+    this->silent = this->sdf->Get<bool>("silent");
+  }
+
+  return this->ParseJoints();
+}
+
+//////////////////////////////////////////////////
+bool ScoringPlugin::ParseJoints()
+{   
+  auto sdf = const_cast<sdf::Element *>(this->sdf.get());
+  // Optional element.
+  if (this->sdf->HasElement("release_joints"))
+  {
+    const sdf::ElementPtr releaseJointsElem = sdf->GetElement("release_joints");
+
+    // We need at least one joint.
+    if (!releaseJointsElem->HasElement("joint"))
+    {
+      ignerr << "Unable to find <joint> element in SDF." << std::endl;
+      return false;
+    }
+
+    auto jointElem = releaseJointsElem->GetElement("joint");
+
+    // Parse a new joint to be released.
+    while (jointElem)
+    {
+      // The joint's name.
+      if (!jointElem->HasElement("name"))
+      {
+        ignerr << "Unable to find <name> element in SDF." << std::endl;
+        return false;
+      }
+
+      const std::string jointName = jointElem->Get<std::string>("name");
+      this->lockJointNames.push_back(jointName);
+
+      // Parse the next joint.
+      jointElem = jointElem->GetNextElement("joint");
+    }
+  }
+
+  return true;
+}
+
+void ScoringPlugin::Exit()
+{
+  bool exit = this->perPluginExitOnCompletion;
+
+  char* env = std::getenv("VRX_EXIT_ON_COMPLETION");
+  if (env != nullptr && std::string(env) == "true")
+  {
+    // Overwrite class variable if environment variable is specified
+    exit = true;
+  }
+
+  if (exit)
+  {
+    // shutdown gazebo
+    ignition::msgs::ServerControl msg;
+    msg.set_stop(true);
+    this->serverControlPub->Publish(msg);
+  }
+  else
+  {
+    ignerr << "VRX_EXIT_ON_COMPLETION and <per_plugin_exit_on_completion> "
+      << "both not set, will not shutdown on ScoringPlugin::Exit()"
+      << std::endl;
+  }
+  return;
+}
+
+void ScoringPlugin::SetTimeoutScore(double _timeoutScore)
+{
+  this->timeoutScore = _timeoutScore;
+}
+
+double ScoringPlugin::GetTimeoutScore() const
+{
+  return this->timeoutScore;
+}
+
+double ScoringPlugin::GetRunningStateDuration() const
+{
+  return this->runningStateDuration;
+}
+
+unsigned int ScoringPlugin::GetNumCollisions() const
+{
+  return this->numCollisions;
+}
+
+
+IGNITION_ADD_PLUGIN(ScoringPlugin,
+                    ignition::gazebo::System,
+                    ScoringPlugin::ISystemConfigure,
+                    ScoringPlugin::ISystemPostUpdate)
---
+++ vrx_ign/src/ScoringPlugin.hh
@@ -0,0 +1,299 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef VRX_GAZEBO_SCORING_PLUGIN_HH_
+#define VRX_GAZEBO_SCORING_PLUGIN_HH_
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <ignition/gazebo/System.hh>
+#include <ignition/common.hh>
+#include <ignition/transport.hh>
+#include <ignition/msgs.hh>
+#include <ignition/physics.hh>
+#include <ignition/math/SphericalCoordinates.hh>
+#include <ignition/gazebo/components/SphericalCoordinates.hh>
+#include <ignition/gazebo/components/Name.hh>
+#include <ignition/gazebo/World.hh>
+#include <sdf/sdf.hh>
+#include <ignition/msgs/any.pb.h>
+#include <ignition/msgs/param.pb.h>
+#include <ignition/msgs/param_v.pb.h>
+#include <ignition/msgs/stringmsg.pb.h>
+#include "ignition/gazebo/Util.hh"
+#include <ignition/transport/Node.hh>
+
+
+class ScoringPlugin 
+        : public ignition::gazebo::System,
+          public ignition::gazebo::ISystemConfigure,
+          public ignition::gazebo::ISystemPostUpdate 
+    {
+        /// \brief Class constructor
+        public: ScoringPlugin();
+
+        /// \brief Shutdown Gazebo and ROS
+        public: void Exit();
+
+        // Documentation inherited
+        public: void Configure(const ignition::gazebo::Entity &_entity,
+                           const std::shared_ptr<const sdf::Element> &_sdf,
+                           ignition::gazebo::EntityComponentManager &_ecm,
+                           ignition::gazebo::EventManager &_eventMgr) override;
+
+        /// \brief Get the current score.
+        /// \return The current score.
+        protected: double Score() const;
+
+        /// \brief Set the score.
+        /// \param[in] _newScore The new score.
+        protected: void SetScore(double _newScore);
+
+        /// \brief Get the task name.
+        /// \return Task name.
+        protected: std::string TaskName() const;
+
+        /// \brief Get the task state.
+        /// \return Task state.
+        protected: std::string TaskState() const;
+
+        /// \brief Elapsed time in the running state.
+        /// \return The elapsed time in the running state.
+        protected: std::chrono::duration<double> ElapsedTime() const;
+
+        /// \brief Remaining time in the running state.
+        /// \return The remaining time in the running state.
+        protected: std::chrono::duration<double> RemainingTime() const;
+
+        /// \brief Finish the current task.
+        /// This will set the "finished" flag in the task message to true.
+        protected: void Finish();
+
+        /// \brief Tries to release the vehicle in case is locked.
+        protected: virtual void ReleaseVehicle();
+
+        /// \brief Set the score in case of timeout
+        protected: void SetTimeoutScore(double _timeoutScore);
+
+        /// \brief Get the timeoutScore
+        protected: double GetTimeoutScore() const;
+
+        /// \brief Get running duration
+        protected: double GetRunningStateDuration() const;
+
+        /// \brief Get the number of WAM-V collisions.
+        protected: unsigned int GetNumCollisions() const;
+
+        /// \brief Callback executed at every world update.
+        public: void PostUpdate(
+                    const ignition::gazebo::UpdateInfo &_info,
+                    const ignition::gazebo::EntityComponentManager &_ecm) override;
+
+        /// \brief Update all time-related variables.
+        std::chrono::duration<double> simTime;
+        private: void UpdateTime(const std::chrono::duration<double> _simTime);
+
+        /// \brief Update the state of the current task.
+        private: void UpdateTaskState();
+
+        /// \brief Update the task stats message.
+        private: void UpdateTaskMessage();
+
+        /// \brief Publish the task stats over a ROS topic.
+        private: void PublishStats();
+
+        /// \brief Callback executed when the task state transition into "ready".
+        private: virtual void OnReady();
+
+        /// \brief Callback executed when the task state transition into "running".
+        private: virtual void OnRunning();
+
+        /// \brief Callback executed when the task state transition into "finished".
+        protected: virtual void OnFinished();
+
+        /// \brief Callback executed when a collision is detected for the WAMV.
+        private: virtual void OnCollision();
+
+        /// \brief Callback function when collision occurs in the world.
+        /// \param[in] _contacts List of all collisions from last simulation iteration
+        private: void OnCollisionMsg(const ignition::msgs::Contacts &_contacts);
+
+        /// \brief Parse all SDF parameters.
+        /// \return True when all parameters were successfully parsed or false
+        /// otherwise.
+        private: bool ParseSDFParameters();
+
+        /// \brief Parse the joints section of the SDF block.
+        /// \return True when all parameters were successfully parsed or false
+        /// otherwise.
+        private: bool ParseJoints();
+
+        /// \brief A world pointer.
+        //TODO: How to define world ptr?
+        // protected: std::shared_ptr<World::World> world;
+        //protected: World world;
+
+          /// \brief Creator interface 
+        protected: std::unique_ptr<ignition::gazebo::SdfEntityCreator> creator{nullptr};
+
+        /// \brief World entity
+        protected: ignition::gazebo::Entity worldEntity{ignition::gazebo::kNullEntity};
+
+        /// \brief Event manager for pausing simulation 
+        public: ignition::gazebo::EventManager *eventManager{nullptr};
+
+        /// \brief The name of the task.
+        protected: std::string taskName = "undefined";
+
+        /// \brief The name of the vehicle to score.
+        protected: std::string vehicleName;
+
+        /// \brief Pointer to the vehicle to score.
+        //TODO: How to define model ptr?
+        // protected: ignition::physics::Model3dPtr vehicleModel;
+        protected: std::unique_ptr<ignition::gazebo::Entity> vehicleModel;
+
+        /// \brief Last collision time.
+        protected: std::chrono::duration<double> lastCollisionTime;
+
+        /// \brief Silent mode enabled?
+        protected: bool silent = false;
+
+        /// \brief Spherical coordinates conversions.
+        protected: ignition::math::SphericalCoordinates sc;
+
+        /// \brief Ignition Transport node.
+        public: ignition::transport::Node node;
+
+        /// \brief Collision detection node subscriber
+        // TODO - Verify if ignition requires a subscriber object
+        
+        /// \brief gazebo server control publisher
+        std::unique_ptr<ignition::transport::Node::Publisher> serverControlPub;
+
+        /// \brief Topic where the task stats are published.
+        private: std::string taskInfoTopic = "/vrx/task/info";
+
+        /// \brief Bool flag for debug.
+        private: bool debug = true;
+
+        /// \brief Topic where debug collision is published.
+        private: std::string contactDebugTopic = "/vrx/debug/contact";
+
+        /// \brief The score.
+        private: double score = 0.0;
+
+        /// \brief Pointer to the SDF plugin element.
+        private: std::shared_ptr<const sdf::Element> sdf; 
+
+        /// \brief Duration (seconds) of the initial state.
+        private: double initialStateDuration = 30.0;
+
+        /// \brief Duration (seconds) of the ready state.
+        private: double readyStateDuration = 60.0;
+
+        /// \brief Duration (seconds) of the running state (max task time).
+        protected: double runningStateDuration = 300.0;
+
+        /// \brief Absolute time specifying the start of the ready state.
+        private: std::chrono::duration<double> readyTime;
+
+        /// \brief Absolute time specifying the start of the running state.
+        private: std::chrono::duration<double> runningTime;
+
+        /// \brief Absolute time specifying the start of the finish state.
+        private: std::chrono::duration<double> finishTime;
+
+        /// \brief Current time (simulation).
+        private: std::chrono::duration<double> currentTime;
+
+        // \brief Elapsed time since the start of the task (running state).
+        private: std::chrono::duration<double> elapsedTime;
+
+        /// \brief Remaining time since the start of the task (running state).
+        private: std::chrono::duration<double> remainingTime;
+
+        /// \brief Collision buffer.
+        private: float collisionBuffer = 3.0;
+
+        /// \brief Collisions counter.
+        private: int collisionCounter = 0;
+
+        /// \brief Collision list.
+        private: std::vector<std::string> collisionList;
+
+        /// \brief Collisions timestamps.
+        private: std::vector<std::chrono::duration<double> > collisionTimestamps;
+
+        /// \brief Whether the current task has timed out or not.
+        private: bool timedOut = false;
+
+        /// \brief Time at which the last message was sent.
+        private: std::chrono::duration<double> lastStatsSent;
+
+        /// \brief The task state.
+        private: std::string taskState = "initial";
+        
+        /// \brief The next task message to be published.
+        // ignition::msgs::Param_V taskMsg;
+        // ignition::msgs::Any taskMsgName, taskMsgState, 
+        //         taskMsgReadyTime, taskMsgRunningTime, taskMsgElapsedTime, taskMsgRemainingTime,
+        //         taskMsgTimedOut, taskMsgNumCollisions, taskMsgScore;
+        
+        // google::protobuf::Map< std::string, ignition::msgs::Any>* taskMsgParam;
+
+        /// \brief Simplified task message (pending protobuf investigation)
+        ignition::msgs::StringMsg taskMessage;
+
+        /// \brief ROS Contact Msg.
+        // private: vrx_ros::msg::Contact contactMsg; //Replaced with ignition messages
+        private: ignition::msgs::Contact contactMsg;
+
+        /// \brief The name of the joints to be dettached during ReleaseVehicle().
+        private: std::vector<std::string> lockJointNames;
+
+        /// \brief Publisher for the task state.
+        //Replaced with ignition messages
+       // protected: std::unique_ptr<ignition::transport::Node::Publisher> taskPub;
+       private: ignition::transport::Node::Publisher taskPub;
+        // protected: rclcpp::Publisher<vrx_ros::msg::Task>::SharedPtr taskPub;
+        // protected: ros::Publisher taskPub;
+
+        /// \brief Publisher for the collision.
+        //Replaced with ignition messages
+        //private: std::unique_ptr<ignition::transport::Node::Publisher> contactPub;
+        private: ignition::transport::Node::Publisher contactPub;
+        // private: rclcpp::Publisher<vrx_ros::msg::Contact>::SharedPtr contactPub;
+        // private: ros::Publisher contactPub;
+
+
+        /// \brief Score in case of timeout - added for Navigation task
+        private: double timeoutScore = -1;
+
+        /// \brief Whether to shut down after last gate is crossed.
+        private: bool perPluginExitOnCompletion = true;
+
+        /// \brief Number of WAM-V collisions.
+        private: unsigned int numCollisions = 0u;
+
+        // For testing
+        private: bool forceReturn = false;
+
+        };
+
+#endif
