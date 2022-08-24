ec59e6b237971a6426498b09e395bb0867bd0efa
M1chaelM <mrmccarr@nps.edu>
2022-08-24 16:00:05 -0700

    not using OpaqueFunction anymore

--- vrx_ros/launch/competition_local.launch.py
+++ vrx_ros/launch/competition_local.launch.py
@@ -17,7 +17,6 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.actions import ExecuteProcess
-from launch.actions import OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
