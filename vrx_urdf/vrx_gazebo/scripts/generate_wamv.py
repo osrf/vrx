9bb7128c1e13253a9270e6ab83b32f3fde46f9ff
Ian Chen <ichen@openrobotics.org>
2022-10-04 20:40:07 -0700

    porting generate_wamv script
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

---
+++ vrx_urdf/vrx_gazebo/scripts/generate_wamv.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from vrx_gazebo.configure_wamv import main
+
+if __name__ == '__main__':
+    main()
---
+++ vrx_urdf/vrx_gazebo/src/vrx_gazebo/configure_wamv.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+import os
+
+from vrx_gazebo.compliance import ComponentCompliance
+from vrx_gazebo.compliance import ThrusterCompliance
+
+from vrx_gazebo.utils import create_xacro_file
+from vrx_gazebo.utils import add_gazebo_thruster_config
+
+
+class ConfigureWAMV(Node):
+
+    def __init__(self):
+        super().__init__('configure_wamv')
+
+        self.declare_parameter('thruster_yaml', '')
+        self.declare_parameter('component_yaml', '')
+        self.declare_parameter('wamv_target', '')
+        self.declare_parameter('wamv_gazebo', '')
+        self.declare_parameter('wamv_locked', '')
+        self.declare_parameter('components_dir', '')
+        self.declare_parameter('thrusters_dir', '')
+
+        # Check if yaml files were given
+        thruster_yaml = self.get_parameter('thruster_yaml').get_parameter_value().string_value
+        component_yaml = self.get_parameter('component_yaml').get_parameter_value().string_value
+        received_thruster_yaml = len(thruster_yaml) > 0
+        received_component_yaml = len(component_yaml) > 0
+
+        thruster_compliant = None
+        component_compliant = None
+
+        # Setup thruster xacro
+        if received_thruster_yaml:
+            thruster_compliant = self.create_thruster_xacro()
+
+        # Setup component xacro
+        if received_component_yaml:
+            component_compliant = self.create_component_xacro()
+
+        # Setup command to generate WAM-V urdf file
+        wamv_target = self.get_parameter('wamv_target').get_parameter_value().string_value
+        wamv_gazebo = self.get_parameter('wamv_gazebo').get_parameter_value().string_value
+        wamv_locked = self.get_parameter('wamv_locked').get_parameter_value().string_value
+
+        create_urdf_command = ("rosrun xacro xacro -o " + wamv_target +
+                               " '" + wamv_gazebo + "'")
+
+        if wamv_locked:
+            create_urdf_command += (" locked:=" +
+                                    str(wamv_locked))
+
+        # Add xacro files if created
+        if received_thruster_yaml:
+            # thruster_yaml = self.get_parameter('thruster_yaml')
+            thruster_xacro_target = os.path.splitext(thruster_yaml)[0] + '.xacro'
+            create_urdf_command += (" yaml_thruster_generation:=true "
+                                    "thruster_xacro_file:=" +
+                                    thruster_xacro_target)
+        if received_component_yaml:
+            # component_yaml = self.get_parameter('component_yaml')
+            component_xacro_target = os.path.splitext(component_yaml)[0] + '.xacro'
+            create_urdf_command += (" yaml_component_generation:=true "
+                                    "component_xacro_file:=" + component_xacro_target)
+
+        # Create urdf and print to console
+        os.system(create_urdf_command)
+        if not (thruster_compliant and component_compliant):
+            rclpy.get_logger().error('\nThis component/thruster configuration is NOT compliant ' +
+                         'with the (current) VRX constraints. A urdf file will ' +
+                         'be created, but please note that the above errors ' +
+                         'must be fixed for this to be a valid configuration ' +
+                         'for the VRX competition.\n')
+
+        print('WAM-V urdf file sucessfully generated. File location: ' +
+              wamv_target)
+
+    def create_thruster_xacro(self):
+        """
+        Purpose: Create a thruster xacro file using the given
+                 rosparameters
+        """
+        # Get yaml files for thruster number and pose
+        thruster_yaml = self.get_parameter('thruster_yaml').get_parameter_value().string_value
+        rclpy.get_logger().info('\nUsing %s as the thruster configuration yaml file\n' %
+                      thruster_yaml)
+
+        # Set thruster xacro target
+        thruster_xacro_target = os.path.splitext(thruster_yaml)[0] + '.xacro'
+        rclpy.get_logger().info('\nTrying to open %s \n' % thruster_xacro_target)
+
+        # Things to start/open the macro
+        thruster_boiler_plate_top = ('<?xml version="1.0"?>\n'
+                                     '<robot '
+                                     'xmlns:xacro="http://ros.org/wiki/xacro" '
+                                     'name="wam-v-thrusters">\n'
+                                     '  <xacro:include filename='
+                                     '"$(find wamv_description)/urdf/thrusters/'
+                                     'engine.xacro" />\n')
+
+        # Things to close the macro
+        thruster_boiler_plate_bot = ''
+
+        # Check if valid number of thrusters and valid thruster parameters
+        thrusters_dir = self.get_parameter('thrusters_dir').get_parameter_value().string_value + '/'
+        comp = ThrusterCompliance(thrusters_dir)
+        thruster_num_test = comp.number_compliance
+        thruster_param_test = comp.param_compliance
+
+        # Create thruster xacro with thruster macros
+        compliant = create_xacro_file(yaml_file=thruster_yaml,
+                                      xacro_target=thruster_xacro_target,
+                                      boiler_plate_top=thruster_boiler_plate_top,
+                                      boiler_plate_bot=thruster_boiler_plate_bot,
+                                      num_test=thruster_num_test,
+                                      param_test=thruster_param_test)
+
+        gz_boiler_plate_top = ('  <gazebo>\n'
+                               '    <plugin name="wamv_gazebo_thrust" '
+                               'filename="libusv_gazebo_thrust_plugin.so">\n'
+                               '      <cmdTimeout>1.0</cmdTimeout>\n'
+                               '      <robotNamespace>${namespace}</robotNamespace>\n'
+                               '      <xacro:include filename="$(find wamv_gazebo)'
+                               '/urdf/thruster_layouts/'
+                               'wamv_gazebo_thruster_config.xacro" />\n')
+        gz_boiler_plate_bot = ('    </plugin>\n'
+                               '  </gazebo>\n'
+                               '</robot>')
+
+        # Append gazebo thruster config to thruster xacro
+        add_gazebo_thruster_config(yaml_file=thruster_yaml,
+                                   xacro_target=thruster_xacro_target,
+                                   boiler_plate_top=gz_boiler_plate_top,
+                                   boiler_plate_bot=gz_boiler_plate_bot,
+                                   )
+        return compliant
+
+
+    def create_component_xacro(self):
+        """
+        Purpose: Create a component xacro file using the given
+                 rosparameters
+        """
+        # Get yaml files for component number and pose
+        component_yaml = self.get_parameter('component_yaml').get_parameter_value().string_value
+        rclpy.get_logger().info('\nUsing %s as the component configuration yaml file\n' %
+                      component_yaml)
+
+        # Set component xacro target
+        component_xacro_target = os.path.splitext(component_yaml)[0] + '.xacro'
+
+        # Things to start/open the macro
+        component_boiler_plate_top = ('<?xml version="1.0"?>\n'
+                                   '<robot '
+                                   'xmlns:xacro="http://ros.org/wiki/xacro" '
+                                   'name="wam-v-components">\n' +
+                                   '  <xacro:macro name="yaml_components">\n')
+
+        # Things to close the macro
+        component_boiler_plate_bot = '  </xacro:macro>\n</robot>'
+
+        # Check if valid number of components and valid component parameters
+        components_dir = self.get_parameter('components_dir').get_parameter_value().string_value + '/'
+        comp = ComponentCompliance(components_dir)
+        component_num_test = comp.number_compliance
+        component_param_test = comp.param_compliance
+
+        # Create component xacro with component macros
+        return create_xacro_file(yaml_file=component_yaml,
+                                 xacro_target=component_xacro_target,
+                                 boiler_plate_top=component_boiler_plate_top,
+                                 boiler_plate_bot=component_boiler_plate_bot,
+                                 num_test=component_num_test,
+                                 param_test=component_param_test)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    configure_wamv  = ConfigureWAMV()
+    rclpy.spin(configure_wamv)
+    configure_wamv.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
