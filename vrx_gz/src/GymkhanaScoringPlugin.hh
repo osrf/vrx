2c1a08bc8454b22d109b222321b7988aca968e5c
M1chaelM <mrmccarr@nps.edu>
2022-11-28 11:24:14 -0800

    add documentation for GymkhanaScoringPlugin

--- vrx_gz/src/GymkhanaScoringPlugin.hh
+++ vrx_gz/src/GymkhanaScoringPlugin.hh
@@ -28,7 +28,44 @@
 
 namespace vrx
 {
-  class GymkhanaScoringPlugin : public ScoringPlugin
+  /// \brief A plugin for computing the score of the gymkhana task.
+  /// This plugin derives from the generic ScoringPlugin class. Refer to that
+  /// plugin for an explanation of the four states defined (Initial, Ready,
+  /// Running and Finished) as well as other required SDF elements.
+  ///
+  /// The gymkhana task is divided into two parts: a navigation channel and
+  /// an obstacle field. The vehicle should first traverse the navigation
+  /// channel, then minimize its distance to an acoustic pinger "blackbox"
+  /// located in the obstacle field.
+  ///  
+  /// This plugin accepts the following optional SDF parameters:
+  ///
+  /// <obstacle_penalty>: Specifies how many points are deducted per collision.
+  /// <pinger_position>: Specifies the location of the pinger in cartesian
+  /// coordinates. The first two coordinates represent cartesian X and Y. These
+  /// default to 0. The third value is always ignored.
+  /// <set_position_topic_name>: Specifies the topic used to set the pinger
+  /// position. The default is /pinger/set_pinger_position. Note this topic is
+  /// for debugging only; the pinger position will not be changed dynamically
+  /// during an actual competition trial.
+  ///
+  /// To manage the navigation channel and obstacle field parts of the task, 
+  /// this plugin also relies on the NavigationScoringPlugin and the
+  /// StationKeepingScoringPlugin. These must be included in the sdf and
+  /// configured according to their documentation, with the following
+  /// additional constraints:
+  /// 
+  /// NavigationScoringPlugin:
+  ///   <per_plugin_exit_plugin_on_completion> must be set to false
+  ///   <silent> is recommended to be set to true to reduce redundant messages 
+  ///
+  /// StationkeepingScoringPlugin:
+  ///   <goal_pose_cart> must have the same value as the <pinger_position>
+  ///   set in the GymKhanaScoringPlugin.
+  ///   <head_error_on> must be set to false
+  ///   <per_plugin_exit_plugin_on_completion> must be set to false
+  ///   <silent> is recommended to be set to true to reduce redundant messages 
+    class GymkhanaScoringPlugin : public ScoringPlugin
   {
     /// \brief Constructor.
     public: GymkhanaScoringPlugin();
