f4ef48828dfcf971d06b39af8d235c6953ae217b
Carlos Agüero <caguero@openrobotics.org>
2022-12-21 18:38:46 +0100

    Enable wind
    Signed-off-by: Carlos Agüero <caguero@openrobotics.org>

--- vrx_gz/scripts/update_sea_state_params.sh
+++ vrx_gz/scripts/update_sea_state_params.sh
@@ -26,14 +26,15 @@ wind_speed=$3
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-# update wave gain param
-find $DIR/../models -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<gain>.*<\/gain>\(.*<\/wavefield\)/\1<gain>'"$wave_gain"'<\/gain>\2/g' {} \;
-find $DIR/../worlds -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<gain>.*<\/gain>\(.*<\/wavefield\)/\1<gain>'"$wave_gain"'<\/gain>\2/g' {} \;
+# Update all wave gain params
+find $DIR/../models -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<gain>[0-9a-z.]\{1,\}</gain>|<gain>'"$wave_gain"'</gain>|g' {} \;
+find $DIR/../worlds -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<gain>[0-9a-z.]\{1,\}</gain>|<gain>'"$wave_gain"'</gain>|g' {} \;
+find $DIR/../../vrx_urdf/wamv_gazebo/urdf/dynamics -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<gain>[0-9a-z.]\{1,\}</gain>|<gain>'"$wave_gain"'</gain>|g' {} \;
 
+# Update all wave period params
+find $DIR/../models -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<period>[0-9a-z.]\{1,\}</period>|<period>'"$wave_period"'</period>|g' {} \;
+find $DIR/../worlds -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<period>[0-9a-z.]\{1,\}</period>|<period>'"$wave_period"'</period>|g' {} \;
+find $DIR/../../vrx_urdf/wamv_gazebo/urdf/dynamics -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<period>[0-9a-z.]\{1,\}</period>|<period>'"$wave_period"'</period>|g' {} \;
 
-# update wave period param
-find $DIR/../models -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<period>.*<\/period>\(.*<\/wavefield\)/\1<period>'"$wave_period"'<\/period>\2/g' {} \;
-find $DIR/../worlds -type f -exec sed -i ':a;N;$!ba;s/\(<wavefield>.*\)<period>.*<\/period>\(.*<\/wavefield\)/\1<period>'"$wave_period"'<\/period>\2/g' {} \;
-
-# update wind linear vel param
+# Update wind linear vel param
 find $DIR/../worlds -type f -exec sed -i ':a;N;$!ba;s/\(<wind>.*\)<linear_velocity>.*<\/linear_velocity>\(.*<\/wind\)/\1<linear_velocity>'"$wind_speed"'<\/linear_velocity>\2/g' {} \;
