5edc79512e109d25f6c3c08a45c16df65b44efb1
Carlos Agüero <caguero@osrfoundation.org>
2023-01-04 19:53:32 +0100

    Do not update world subdirectories.
    Signed-off-by: Carlos Agüero <caguero@osrfoundation.org>

--- vrx_gz/scripts/update_sea_state_params.sh
+++ vrx_gz/scripts/update_sea_state_params.sh
@@ -28,13 +28,13 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # Update all wave gain params
 find $DIR/../models -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<gain>[0-9a-z.]\{1,\}</gain>|<gain>'"$wave_gain"'</gain>|g' {} \;
-find $DIR/../worlds -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<gain>[0-9a-z.]\{1,\}</gain>|<gain>'"$wave_gain"'</gain>|g' {} \;
+find $DIR/../worlds -maxdepth 1 -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<gain>[0-9a-z.]\{1,\}</gain>|<gain>'"$wave_gain"'</gain>|g' {} \;
 find $DIR/../../vrx_urdf/wamv_gazebo/urdf/dynamics -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<gain>[0-9a-z.]\{1,\}</gain>|<gain>'"$wave_gain"'</gain>|g' {} \;
 
 # Update all wave period params
 find $DIR/../models -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<period>[0-9a-z.]\{1,\}</period>|<period>'"$wave_period"'</period>|g' {} \;
-find $DIR/../worlds -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<period>[0-9a-z.]\{1,\}</period>|<period>'"$wave_period"'</period>|g' {} \;
+find $DIR/../worlds -maxdepth 1 -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<period>[0-9a-z.]\{1,\}</period>|<period>'"$wave_period"'</period>|g' {} \;
 find $DIR/../../vrx_urdf/wamv_gazebo/urdf/dynamics -type f -exec sed -i  '/<wavefield>/,/<\/wavefield>/ s|<period>[0-9a-z.]\{1,\}</period>|<period>'"$wave_period"'</period>|g' {} \;
 
 # Update wind linear vel param
-find $DIR/../worlds -type f -exec sed -i ':a;N;$!ba;s/\(<wind>.*\)<linear_velocity>.*<\/linear_velocity>\(.*<\/wind\)/\1<linear_velocity>'"$wind_speed"'<\/linear_velocity>\2/g' {} \;
+find $DIR/../worlds -maxdepth 1 -type f -exec sed -i ':a;N;$!ba;s/\(<wind>.*\)<linear_velocity>.*<\/linear_velocity>\(.*<\/wind\)/\1<linear_velocity>'"$wind_speed"'<\/linear_velocity>\2/g' {} \;
