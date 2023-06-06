496f920252c482c57aaf42090c6616e3ee7bb238
Jessica Herman <jessica.herman@nps.edu>
2023-06-06 09:01:34 -0700

    New frame for RVIZ visuals

--- vrx_urdf/vrx_gazebo/launch/rviz.launch.py
+++ vrx_urdf/vrx_gazebo/launch/rviz.launch.py
@@ -10,8 +10,6 @@ import os.path
 def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    #urdf_file = os.path.join(get_package_share_directory('vrx_gazebo'), 'models', 'wamv', 'tmp', 'model.urdf')
-    #urdf_file = '/home/jessica/vrx_colcon_ws/src/vrx/vrx_urdf/wamv_gazebo/urdf/wamv_ground_truth.urdf'
     urdf_file = os.path.join(get_package_share_directory('wamv_gazebo'), 'urdf/wamv_ground_truth.urdf')
     with open(urdf_file, 'r') as infp:
         robot_desc = infp.read()
@@ -34,7 +32,6 @@ def generate_launch_description():
             namespace='',
             executable='rviz2',
             name='rviz2',
-            #parameters={'use_sim_time': 'true'},
             arguments=['-d' + os.path.join(get_package_share_directory('vrx_gazebo'), 'config', 'rviz_vrx_final.rviz')]
         )
     return LaunchDescription([rsp,jsp,rvz])
