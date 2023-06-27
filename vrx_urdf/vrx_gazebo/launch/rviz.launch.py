340c96ddf6ed95e16f2e6db5b2f92b8a75f82d8d
Jessica Herman <jessica.herman@nps.edu>
2023-06-27 12:33:04 -0700

    Adds robot_state_publisher to provide missing static transforms (temp fix)

--- vrx_urdf/vrx_gazebo/launch/rviz.launch.py
+++ vrx_urdf/vrx_gazebo/launch/rviz.launch.py
@@ -14,10 +14,14 @@ def generate_launch_description():
     urdf_file = os.path.join(model_dir, 'model.urdf')
     with open(urdf_file, 'r') as infp:
         robot_desc = infp.read()
-    params = {'robot_description': robot_desc}
+    DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true'),
+    params = {'use_sim_time': use_sim_time, 'frame_prefix': 'wamv/', 'robot_description': robot_desc, 'publish_frequency': 0.001}
 
-    rdp = Node(package='vrx_gazebo',
-                                  executable='robot_desc_publisher.py',
+    rsp = Node(package='robot_state_publisher',
+                                  executable='robot_state_publisher',
                                   output='both',
                                   parameters=[params])
     rvz = Node(
@@ -27,4 +31,4 @@ def generate_launch_description():
             name='rviz2',
             arguments=['-d' + os.path.join(get_package_share_directory('vrx_gazebo'), 'config', 'rviz_vrx_final.rviz')]
         )
-    return LaunchDescription([rdp,rvz])
+    return LaunchDescription([rsp,rvz])
