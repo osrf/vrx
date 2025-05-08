fe401634e17eff5debd66d20c70032e6258e958a
Carlos Agüero <caguero@openrobotics.org>
2025-05-08 19:51:13 +0200

    Add Honu Robotics headers to copyrights
    Signed-off-by: Carlos Agüero <caguero@openrobotics.org>

--- vrx_gz/src/WayfindingScoringPlugin.cc
+++ vrx_gz/src/WayfindingScoringPlugin.cc
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2019 Open Source Robotics Foundation
+ * Copyright (C) 2025 Honu Robotics
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
